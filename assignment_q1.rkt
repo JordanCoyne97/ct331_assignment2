3d8e379760ee9bbefdc62c86b1eea32aa58e38ab
Jordan Coyne <jordancoyne@hotmail.com>
2018-11-02 10:41:30 +0000

    q1

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,10 @@
 #lang racket
+(cons 1 2)
+
+(cons 1 (cons 2 (cons 3 empty)))
+
+(cons "para" (cons 1 (cons (cons 1 (cons 2 (cons 3 empty))) '())))
+
+(list "para" '1 (list 2 3 4 ))
+
+(append '("para") '(1) '((3 4 5)))
