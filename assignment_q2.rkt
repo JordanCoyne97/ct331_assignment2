4a7c034a0a665bcec0eefcb9f0f1a9836cfbbd10
Jordan Coyne <jordancoyne@hotmail.com>
2018-11-08 10:35:53 +0000

    q2

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,56 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
+;A
 (provide ins_beg)
+(define (ins_beg element list)
+  (cons element list))
 
-(define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+;B
+(provide ins_end)
+(define (ins_end element list)
+  (append list (list element)))
+
+
+;C
+(provide cout_top_level)
+(define (cout_top_level list)
+    (cond [(null? list) 0]
+     
+    [else (+ 1 (cout_top_level (cdr list)))]))
+
+;D
+(provide count_instances)
+(define (count_instances atomic list)
+  
+   (cond ((null? list)0)
+         
+   ((= atomic (car list))(+ 1(count_instances atomic (cdr list))))
+      
+   ((count_instances atomic (cdr list)))
+   ))
+
+
+;E
+(provide count_instances_tr)
+(define (count_instances_tr atomic list)
+
+  (count_instances_tr atomic list 0))
+
+  (define (helper_count_instances_tr item lst count)
+  (cond [(empty? lst) count]
+        [(equal? item (car lst)) (helper_count_instances_tr item (cdr lst) (+ 1 count))]
+        [else (helper_count_instances_tr item (cdr lst) count)]))
+  
+;F
+(provide count_instances_deep)
+(define (count_instances_deep atomic list)
+  
+  (cond [(empty? list) 0]
+        
+  [(list? (car list)) (+ (count_instances_deep atomic (car list)) (count_instances_deep atomic (cdr list))) ]
+     
+  [(equal? atomic (car list)) (+  1 (count_instances_deep atomic (cdr list)))]
+        
+  [else (count_instances_deep atomic (cdr list))]))
+
+  
